778de642e689ea4d53f26c1354151e9a25c85972
Yuri Suzuki <lil.yf.bb7v@gmail.com>
2020-09-23 00:00:07 +0900

    ルート追加

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+Route::get('/', 'ArticleController@index');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
